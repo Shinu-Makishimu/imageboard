3c78e3fcfaf73d465d153a8c73031ec00c5e732c
Makishimu <anon.dostavlaet@gmail.com>
2022-09-30 18:39:43 +0200

    next commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,53 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::Vector;
+use near_sdk::collections::{Vector, UnorderedMap};
+//use near_sdk::serde::{Serialize, Deserialize};
 use near_sdk::{near_bindgen, AccountId, env};
 
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
+#[derive(Debug)]
 pub struct Thread {
     pub author: AccountId,
     pub text: String,
     pub is_closed: bool,
 //    pub answers: Vector<Answers>,
 }
-
 /*
 pub struct Answers {
-    pub posts: ???
+    pub posts: 
 }
 */
 
 
+
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
+//#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Debug, PartialEq)]
+#[derive(BorshDeserialize, BorshSerialize,)]
+//#[serde(crate = "near_sdk::serde")]
 pub struct ImageBoard {
-    threads: Vector<Thread>,
+    threads: UnorderedMap<i32, Thread>,
     owner: AccountId,
     moderators: Vector<AccountId>,
+    threads_count: i32,
+
 }
 
 
 impl Default for ImageBoard{
     fn default() -> Self {
         let owner = env::predecessor_account_id();
+        let threads_count: i32 = 0;
         Self { 
-            threads: Vector::new(b"i"), 
+            threads: UnorderedMap::new(b"s"), 
             owner, 
             moderators: Vector::new(b"m"),
+            threads_count,
         
         }
     }
 }
+
 
 
 #[near_bindgen]
@@ -46,16 +55,23 @@ impl ImageBoard{
     pub fn add_thread(&mut self, text: String) {
         let is_closed: bool = false;
         let author = env::predecessor_account_id();
+        self.threads_count += 1;
+        let threads_count = self.threads_count;
 
         let message = Thread{author, text, is_closed};
 
-        self.threads.push(&message);
+        self.threads.insert(&threads_count, &message);
     }
 
-    pub fn get_threads(&self) -> Vec<Thread> {
-        self.threads.iter().take(50 as usize).collect()
+    pub fn get_threads(&self) -> Vec<(i32, Thread)> {
+        self.threads.to_vec()     
+    }
+
+    pub fn add_moder(){
+
     }
 }
+
 
 
 
@@ -67,8 +83,13 @@ mod tests {
     fn add_thread() {
         let mut contract = ImageBoard::default();
         contract.add_thread("there are all dead".to_string());
-        let thread = &contract.get_threads()[0];
-        
-        assert_eq!(thread.text, "there are all dead".to_string())
+        contract.add_thread("there are all dead".to_string());
+        contract.add_thread("there are all dead".to_string());
+
+        let thread = &contract.get_threads()[2];
+        let (key, sample) = thread;
+        let one = 3;
+        assert_eq!(key, &one);
+        assert_eq!(sample.text, "there are all dead".to_string());
     }
 }

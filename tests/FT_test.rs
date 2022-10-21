c7af1c0708130d6e723da43dbb46641a972cd6c1
Makishimu <anon.dostavlaet@gmail.com>
2022-10-21 15:15:43 +0200

    ыф

--- tests/FT_test.rs
+++ tests/FT_test.rs
@@ -2,42 +2,38 @@
 use near_sdk::json_types::U128;
 use near_sdk::{log};
 use serde_json::json;
+use workspaces::result::ExecutionResult;
 use std::convert::TryInto;
 
 use anyhow::Ok;
 use near_units::{parse_gas, parse_near};
 use workspaces::network::Sandbox;
-use workspaces::{Account, AccountId, Contract, Worker};
+use workspaces::{Account, AccountId, Contract, Worker, DevNetwork};
 use workspaces::BlockHeight;
 
 const WASM_FILEPATH: &str = "imageboard.wasm";
 
-const BLOCK_HEIGHT: BlockHeight = 103_031_309;
+const BLOCK_HEIGHT: BlockHeight = 50_000_000;
 
+async fn create_testname(
+    owner: &Account,
+    worker: &Worker<impl DevNetwork>,
+) -> anyhow::Result<Contract> {
+    let gp: Contract = worker
+        .dev_deploy(&std::fs::read(WASM_FILEPATH)?)
+        .await?;
 
-/*async fn deploy(
-        owner: &Account,
-        worker: &Worker<impl DevNetwork>,
-    )-> anyhow::Result<Contract>  {
+    gp.call("new")
+        .args_json(json!({
+            "owner": owner.id(),
+        }))
+        .transact()
+        .await?
+        .into_result()?;
+    ();
 
-    let wasm: Vec<u8> = std::fs::read(WASM_FILEPATH).unwrap();
-
-    let ib: Contract = worker.
-                        dev_deploy(&wasm).
-                        await?;
-
-    ib.
-        call("new").
-        args_json(serde_json::json!({
-            "owner": owner.id()
-        })).
-        transact().
-        await?.
-        into_result()?;
-
-    Ok(ib)
-
-}*/
+    Ok(gp)
+}
 
 
 async fn create_wnear(
@@ -67,7 +63,7 @@ async fn create_wnear(
     owner
         .call(wnear.id(), "storage_deposit")
         .args_json(json!({}))
-        .deposit(parse_near!("0.008 N"))
+        .deposit(parse_near!("0.00125 N"))
         .transact()
         .await?
         .into_result()?;
@@ -78,9 +74,11 @@ async fn create_wnear(
         .transact()
         .await?
         .into_result()?;
-
     Ok(wnear)
 }
+
+
+
 
 #[tokio::test]
 async fn test_deploy() -> anyhow::Result<()> {
@@ -89,24 +87,38 @@ async fn test_deploy() -> anyhow::Result<()> {
     let owner: Account = worker.root_account()?;
     //let owner_id = owner.id();
     
-    let wasm: Vec<u8> = std::fs::read(WASM_FILEPATH).unwrap();
+    //let wasm: Vec<u8> = std::fs::read(WASM_FILEPATH).unwrap();
 
     let wnear: Contract = create_wnear(&owner, &worker).await?;
-    //let imageboard: Contract = deploy(&owner, &worker);
-    
+    let imageboard: Contract = create_testname(&owner, &worker).await?;
+    /*let imageboard: Contract = worker.
+                                dev_deploy(&wasm).
+                                await?;*/
 
-    let imageboard: Contract = worker.
-                        dev_deploy(&wasm).
-                        await?;
+    let register_result = owner
+        .call(wnear.id(), "storage_deposit")
+        .args_json(json!({
+            "account_id": imageboard.id(),
+            "registration_only": true,
+        }))
+        .deposit(parse_near!("0.00125 N"))
+        .transact()
+        .await?
+        .into_result()?;
 
-    imageboard.
+
+
+
+
+
+    /*imageboard.
         call("new").
         args_json(serde_json::json!({
             "owner": owner.id()
         })).
         transact().
         await?.
-        into_result()?;
+        into_result()?;*/
 
     let owner_id: AccountId = imageboard 
         .call("get_owner")
@@ -118,13 +130,13 @@ async fn test_deploy() -> anyhow::Result<()> {
     log!("from account {:?}", owner.id());
 
 
-
+    log!("wnear22{:?}", wnear.id());
     /*let transfer = owner.
                     call(wnear.id(), "ft_transer_call").
                     args_json(json!({
                         "receiver_id": owner_id,
                         "amount": parse_near!("420 N").to_string(),
-                        "msg": "idk0what1i2must3write4here",
+                        "msg": "idk0what1i2must3write4here".to_string(),
                     })).
                     gas(parse_gas!("200 Tgas") as u64).
                     deposit(1).
@@ -132,10 +144,11 @@ async fn test_deploy() -> anyhow::Result<()> {
                     await?.
                     into_result()?;*/
 
-    let transfer = wnear.
-                    call("ft_transer_call").
+
+    let transfer2 = wnear.
+                    call("ft_transfer_call").
                     args_json(json!({
-                        "receiver_id": owner.id(),
+                        "receiver_id": imageboard.id(),
                         "amount": parse_near!("420 N").to_string(),
                         "msg": "idk0what1i2must3write4here",
                     })).
